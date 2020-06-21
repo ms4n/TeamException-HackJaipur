d0ae907bcda9499fe5b417ffd66554905fd6258b
sanjay-sw <arjunsanjay0@gmail.com>
2020-06-21 11:50:12 +0530

    Added login and signup activities

--- app/src/main/java/com/example/exception/MainActivity.java
+++ app/src/main/java/com/example/exception/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.exception;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.login_button);
+        signupBtn = findViewById(R.id.signup_button);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+                finish();
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Signup.class));
+                finish();
+            }
+        });
     }
 }
